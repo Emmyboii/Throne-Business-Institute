94b65eaed1f565229d06d36026b9e1e8d59fda40
Olukoya Emmanuel <olukoyaemmanuel@example.com>
2024-08-20 02:30:05 +0100

    A

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -48,33 +48,33 @@ const Navbar = () => {
                     <a href='/' ><img onClick={() => setMenu('home')} src={Logo} alt='' className='xl:w-[150px] lg:w-[140px] md:w-[120px] w-[100px]' /></a>
                 </div>
                 <ul className='hidden lg:flex xl:text-[21px] lg:text-[17px] ml-[30%] gap-[25px] items-center'>
-                    <Link to='/'>
+                    <a href='/'>
                         <li onClick={() => setMenu('home')} className={menu === "home" ? 'text-[#ffc936] ' : 'active:scale-95 hover:text-[#ffc936]'}>
                             Home
                         </li>
-                    </Link>
-                    <Link to='/about'>
+                    </a>
+                    <a href='/about'>
                         <li onClick={() => setMenu('about')} className={menu === "about" ? 'text-[#ffc936]' : 'active:scale-95 hover:text-[#ffc936]'}>
                             About Us
                         </li>
-                    </Link>
-                    <Link to='/programs'>
+                    </a>
+                    <a href='/programs'>
                         <li onClick={() => setMenu('program')} className={menu === "program" ? 'text-[#ffc936]' : 'active:scale-95 hover:text-[#ffc936]'}>
                             Programs
                         </li>
-                    </Link>
-                    <Link to='/accreditation'>
+                    </a>
+                    <a href='/accreditation'>
                         <li onClick={() => setMenu('Accredite')} className={menu === "Accredite" ? 'text-[#ffc936]' : 'active:scale-95 hover:text-[#ffc936]'}>
                             Accreditation
                         </li>
-                    </Link>
+                    </a>
                 </ul>
                 <div className='md:flex md:items-center hidden'>
-                    <Link to='/contact'>
+                    <a href='/contact'>
                         <button onClick={() => setMenu('Contact')} className='bg-blue-900  text-white active:scale-95 mt-2  py-3 px-6 rounded-lg outline-none active:bg-blue-800'>
                             Contact Us
                         </button>
-                    </Link>
+                    </a>
                 </div>
                 <div onClick={handleNav}>
                     <HiMenuAlt3 className='md:flex lg:hidden text-[30px] mt-[12px] md:mt-[20px] mr-7 cursor-pointer' />
@@ -90,31 +90,31 @@ const Navbar = () => {
                 <AiOutlineClose onClick={handleNav} size={30} className='absolute right-4 sb:top-[40px] top-[30px] cursor-pointer' />
                 <nav className='lg:hidden flex flex-col'>
                     <ul className='flex flex-col p-4'>
-                        <Link to='/' onClick={handleNav}>
+                        <a href='/' onClick={handleNav}>
                             <li onClick={() => setMenu('home')} className={menu === "home" ? 'text-red-600  p-3 rounded-xl bg-slate-800' : 'p-3 rounder-xl text-white active:scale-95 hover:text-red-600'}>
                                 Home
                             </li>
-                        </Link>
-                        <Link to='/about' onClick={handleNav}>
+                        </a>
+                        <a href='/about' onClick={handleNav}>
                             <li onClick={() => setMenu('about')} className={menu === "about" ? 'text-red-600  rounded-xl p-3 bg-slate-800' : 'p-3  rounded-xl text-white active:scale-95 hover:text-red-600'}>
                                 About Us
                             </li>
-                        </Link>
-                        <Link to='/programs' onClick={handleNav}>
+                        </a>
+                        <a href='/programs' onClick={handleNav}>
                             <li onClick={() => setMenu('program')} className={menu === "program" ? 'text-red-600  rounded-xl p-3 bg-slate-800' : 'p-3  rounded-xl text-white active:scale-95 hover:text-red-600'}>
                                 Programs
                             </li>
-                        </Link>
-                        <Link to='/accreditation' onClick={handleNav}>
+                        </a>
+                        <a href='/accreditation' onClick={handleNav}>
                             <li onClick={() => setMenu('Accredite')} className={menu === "Accredite" ? 'text-red-600 rounded-xl p-3 bg-slate-800' : 'p-3  rounded-xl text-white active:scale-95 hover:text-red-600'}>
                                 Accreditation
                             </li>
-                        </Link>
-                        <Link to='/contact' onClick={handleNav}>
+                        </a>
+                        <a href='/contact' onClick={handleNav}>
                             <button onClick={() => setMenu('Contact')} className='bg-blue-900 text-white active:scale-95 mt-2 w-full py-3 px-6 rounded-lg outline-none active:bg-blue-800'>
                                 Contact Us
                             </button>
-                        </Link>
+                        </a>
                     </ul>
                 </nav>
             </div>
